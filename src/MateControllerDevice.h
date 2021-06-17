f573e900a22ab8f56c0da1b77b1d22db27c54af6
Jared Sanson <jared@jared.geek.nz>
2021-06-17 22:04:07 +1200

    expose port / isConnected property getters

--- src/MateControllerDevice.h
+++ src/MateControllerDevice.h
@@ -5,20 +5,20 @@ class MateControllerDevice {
 public:
     MateControllerDevice(
         MateControllerProtocol& protocol, 
-        DeviceType dtype
+        DeviceType m_dtype
     ) : 
         protocol(protocol),
-        dtype(dtype),
-        port(-1),
-        is_open(false)
+        m_dtype(m_dtype),
+        m_port(-1),
+        m_isOpen(false)
     { }
 
-    // Open a device on the specified port.
-    // returns false if the device is not present on that port.
+    // Open a device on the specified m_port.
+    // returns false if the device is not present on that m_port.
     bool begin(uint8_t port) {
-        this->port = port;
-        is_open = (protocol.scan(port) == dtype);
-        return is_open;
+        this->m_port = port;
+        m_isOpen = (protocol.scan(m_port) == m_dtype);
+        return m_isOpen;
     }
 
     // Query a register and retrieve its value (BLOCKING)
@@ -26,38 +26,44 @@ public:
     // param:    Optional parameter
     // returns:  The register value
     uint16_t query(uint16_t reg, uint16_t param = 0) {
-        return protocol.query(reg, param, this->port);
+        return protocol.query(reg, param, this->m_port);
     }
 
     // Control something (BLOCKING)
     // reg:      The control address
     // value:    The value to use for controlling (eg. disable/enable)
     void control(uint16_t reg, uint16_t value) {
-        protocol.control(reg, value, this->port);
+        protocol.control(reg, value, this->m_port);
     }
 
     // Read the revision from the target device
     revision_t get_revision() {
-        if (dtype == DeviceType::Fx) {
+        if (m_dtype == DeviceType::Fx) {
             // FX devices use an alternate method for retrieving revision
             revision_t rev = {0};
             rev.c = query(0x0001);
             return rev;
         }
         else {
-            return protocol.get_revision(this->port);
+            return protocol.get_revision(this->m_port);
         }
     }
 
     DeviceType deviceType() const {
-        return dtype;
+        return m_dtype;
+    }
+    int8_t port() const {
+        return m_port;
+    }
+    bool isConnected() const {
+        return m_isOpen;
     }
 
 protected:
     MateControllerProtocol& protocol;
-    DeviceType dtype;
-    int8_t port;
-    bool is_open;
+    DeviceType m_dtype;
+    int8_t m_port;
+    bool m_isOpen;
 };
 
 #endif /* MATE_CONTROLLER_DEVICE_H */
